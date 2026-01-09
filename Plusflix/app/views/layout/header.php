2e7f1bfc6cb6767564fbdda5b5d1f78cdace2073
miikomiikomii <mgolebiz@gmail.com>
2026-01-09 19:14:01 +0100

    Update header.php
    Naprawiony header, zaimplementowano uprzednio niepoprawne wyświetlanie strony

--- Plusflix/app/views/layout/header.php
+++ Plusflix/app/views/layout/header.php
@@ -1,17 +1,17 @@
 <!doctype html>
 <html lang="pl">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PLUSFIX</title>
-    <link rel="stylesheet" href="public/css/style.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>PLUSFLIX • KM1</title>
+  <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
-    <header class="topbar">
-        <a class="brand" href="<?= url('titles','index') ?>">PLUSFLIX</a>
-        <nav class="nav">
-            <a href="<?= url('titles','create') ?>">Dodaj tytuł</a>
-            <a href="<?= url('setup','reset') ?>" onclick="return confirm('Zresetować bazę?')">Reset DB</a>
-        </nav>
-    </header>
-    <main class="container>
+<header class="topbar">
+  <a class="brand" href="<?= url('titles','index') ?>">PLUSFLIX</a>
+  <nav class="nav">
+    <a href="<?= url('titles','create') ?>">Dodaj tytuł</a>
+    <a href="<?= url('setup','reset') ?>" onclick="return confirm('Zresetować bazę?')">Reset DB</a>
+  </nav>
+</header>
+<main class="container">
