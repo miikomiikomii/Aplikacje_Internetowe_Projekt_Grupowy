0467e56d9c79fdb8cc4e05393dd6096e797a370e
PossessedScarf <sub01empty@gmail.com>
2026-01-23 18:33:22 +0100

    Header fix

--- Plusflix/app/views/layout/header.php
+++ Plusflix/app/views/layout/header.php
@@ -1,17 +1,22 @@
 <!doctype html>
 <html lang="pl">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>PLUSFLIX • KM1</title>
-  <link rel="stylesheet" href="public/css/style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PLUSFLIX • KM3</title>
+    <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
 <header class="topbar">
-  <a class="brand" href="<?= url('titles','index') ?>">PLUSFLIX</a>
-  <nav class="nav">
-    <a href="<?= url('titles','create') ?>">Dodaj tytuł</a>
-    <a href="<?= url('setup','reset') ?>" onclick="return confirm('Zresetować bazę?')">Reset DB</a>
-  </nav>
+    <a class="brand" href="<?= url('titles','index') ?>">PLUSFLIX</a>
+    <nav class="nav">
+        <?php if (isAdmin()): ?>
+            <a href="<?= url('titles','create') ?>">Dodaj tytuł</a>
+            <a href="<?= url('admin','dashboard') ?>">Panel admina</a>
+            <a href="<?= url('admin','logout') ?>">Wyloguj</a>
+        <?php else: ?>
+            <a href="<?= url('admin','login') ?>">Admin</a>
+        <?php endif; ?>
+    </nav>
 </header>
 <main class="container">
